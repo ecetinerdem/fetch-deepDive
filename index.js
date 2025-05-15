cfd4f10eaaad47bb39eab125328e35f74461a3f0
Cetin Erdem <cetoerdem1@gmail.com>
2025-05-15 12:47:53 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// API - Application Programming Interface: software to communicate with other software
+// API - helpful service
+// REST API
+
+// CRUD 
+// create POST
+// read GET
+// update PUT / PATCH
+// delete DELETE
+
+// http://jsonplaceholder.typicode.com/posts
+// JSON - JavaScript Object Notation
+// fetch(url)
+// jsonplaceholder.typicode.com
+
+// GET /posts/1 - single blog post
+// POST /posts
+const blogPost = {
+  title: "Cool post",
+  body: "lkajsdflkjasjlfda",
+  userId: 1  
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: "POST",
+  headers: {
+     "Content-Type": "application/json" 
+  },
+  body: JSON.stringify(blogPost)
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
